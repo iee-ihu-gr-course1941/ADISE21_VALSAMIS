41c15aacbe1c5a32c2ad071493c1d1716aadfe2f
tony409val <57224825+tony409val@users.noreply.github.com>
2021-12-16 18:14:27 +0200

    create quarto.php
    Request handlers

---
+++ quarto.php
@@ -0,0 +1,146 @@
+<?php
+
+require_once "lib/dbconnect.php";
+require_once "lib/users.php";
+require_once "lib/board.php";
+require_once "lib/status.php";
+
+/**
+ * stores http request method  to local variable
+ * String handling to get the path contents as a table without white space in front and back 
+ * store http request body raw data using json format to local variable
+ */
+
+$method = $_SERVER['REQUEST_METHOD'];
+$request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
+$input = json_decode(file_get_contents('php://input'), true);
+
+if (isset($_SERVER['HTTP_X_TOKEN'])) {
+        $input['token'] = $_SERVER['HTTP_X_TOKEN'];
+}
+
+/**
+ * The following switch controls and redirects the proper actions to be made as specified with the http request 
+ * using $request can identify the path levels  
+ * using $method can identify the type of the action to be made at the specific path level 
+ * using $input can read and pass as parameters raw data from the http request body 
+ */
+
+switch ($r = array_shift($request)) {
+        case 'board':
+                switch ($b = array_shift($request)) {
+                        case '':
+                        case null:
+                                handle_board($method, $input);
+                                break;
+                        case 'piece':
+                                handle_piece($method, $request[0], $input);
+                                break;
+                        default:
+                                header("HTTP/1.1 404 Not Found");
+                                break;
+                }
+                break;
+        case 'status':
+                handle_status($method);
+                break;
+        case 'players':
+                handle_player($method, $request, $input);
+                break;
+
+        default:
+                header("HTTP/1.1 404 Not Found");
+                exit;
+}
+
+/**
+ * selects the appropriate board action(function call ) acording http request  
+ * @param string $method can be either GET or POST 
+ * @param json $input  body of http request
+ * 
+ */
+
+function handle_board($method, $input)
+{
+        if ($method == 'GET') {
+                show_board($input);
+        } else if ($method == 'POST') {
+                reset_board();
+                show_board($input);
+        }
+}
+
+/**
+ * selects the appropriate piece action(function call ) acording http request  
+ * @param string $method can be either GET or PUT
+ * @param json $input  body of http request
+ * 
+ */
+
+function handle_piece($method, $request, $input)
+{
+        switch ($request) {
+                case 'pick':
+                        if ($method == 'PUT') {
+                                pick_piece($input);
+                        } else {
+                                piece_list();
+                        }
+                        break;
+                case 'place':
+                        if ($method == 'PUT') {
+                                place_piece($input);
+                        } else {
+                                header("HTTP/1.1 400 Bad Request");
+                                print json_encode(['errormesg' => "Method $method not allowed here."]);
+                        }
+                        break;
+        }
+}
+
+/**
+ * selects the appropriate player action(function call ) according to the http request  
+ * @param array $request contains the http request path as a table with its level on a cell 
+ * @param string $method can be either GET or PUT
+ * @param json $input  body of http request
+ * 
+ */
+
+function handle_player($method, $request, $input)
+{
+        switch ($b = array_shift($request)) {
+                case '':
+                case null:
+                        if ($method == 'GET') {
+                                show_users($method);
+                        } else {
+                                header("HTTP/1.1 400 Bad Request");
+                                print json_encode(['errormesg' => "Method $method not allowed here."]);
+                        }
+                        break;
+                case 'login':
+                        handle_user($method, $input);
+                        break;
+                default:
+                        header("HTTP/1.1 404 Not Found");
+                        print json_encode(['errormesg' => "Player $b not found."]);
+                        break;
+        }
+}
+
+/**
+ * selects the appropriate status action(function call ) acording http request  
+ * @param string $method only GET is implemented
+ * 
+ */
+
+function handle_status($method)
+{
+        if ($method == 'GET') {
+                show_status();
+        } else {
+                header("HTTP/1.1 400 Bad Request");
+                print json_encode(['errormesg' => "Method $method not allowed here."]);
+        }
+}
+?>
